6feab46912d85114d0bb9025968cf6e9a2f11d33
lampaBiurkowa <mrocznaklawiatura@gmail.com>
2024-10-26 22:55:46 +0200

    :|

--- DinBase/Options/DibBaseOptions.cs
+++ DinBase/Options/DibBaseOptions.cs
@@ -4,9 +4,5 @@ public class DsDbLibOptions
 {
     public const string SECTION = "DsDbLib";
 
-    public required string ObfuscationKey { get; set; }
-    public required string DatabaseName { get; set; }
-    public required string Password { get; set; }
-    public required string User { get; set; }
-    public required string Host { get; set; }
+    public required string ConnectionString { get; set; }
 }
