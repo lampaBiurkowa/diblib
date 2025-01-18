2e666ef7642b277e7ce3645e9f2e23f61beadcc9
lampaBiurkowa <mrocznaklawiatura@gmail.com>
2025-01-18 22:23:33 +0100

    igrator

---
+++ DibBaseApi/IServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+using System.Data.Common;
+using DibBase.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DibBase.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static async Task<bool> MigratePostgresIfRequested<T>(
+        this IServiceCollection services,
+        IConfiguration conf,
+        string dbName,
+        DbConnection connection
+    ) where T : DbContext
+    {   
+        var dbOptions = conf.GetSection(DsDbLibOptions.SECTION).Get<DsDbLibOptions>();
+        if (dbOptions != null && dbOptions.Migrate)
+        {
+            await connection.OpenAsync();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"SELECT 1 FROM pg_database WHERE datname = '{dbName}'";
+            var exists = await command.ExecuteScalarAsync() != null;
+            if (!exists)
+            {
+                command.CommandText = $"CREATE DATABASE \"{dbName}\"";
+                await command.ExecuteNonQueryAsync();
+            }
+            await connection.CloseAsync();
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<T>();
+            await context.Database.MigrateAsync();
+            return true;
+        }
+
+        return dbOptions != null && dbOptions.Migrate;
+    }
+}
--- DinBase/Options/DibBaseOptions.cs
+++ DinBase/Options/DibBaseOptions.cs
@@ -8,6 +8,7 @@ public class DsDbLibOptions
     public required string User { get; set; }
     public required string Host { get; set; }
     public required int Port { get; set; }
+    public required bool Migrate { get; set; }
 
     public string GetConnectionString(string dbName) =>
         $"User ID={User};Password={Password};Host={Host};Port={Port};Database={dbName};";
