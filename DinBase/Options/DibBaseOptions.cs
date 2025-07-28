da9bcd05316903a047413e967a58406de0b2e662
Piotr Walendziak <piotr.walendziak@freymedical.eu>
2025-07-28 17:06:48 +0200

    testtest

--- DinBase/Options/DibBaseOptions.cs
+++ DinBase/Options/DibBaseOptions.cs
@@ -9,7 +9,8 @@ public class DsDbLibOptions
     public required string Host { get; set; }
     public required int Port { get; set; }
     public required bool Migrate { get; set; }
+    public int CommandTimeout { get; set; } = 10;
 
     public string GetConnectionString(string dbName, string schema) =>
-        $"User ID={User};Password={Password};Host={Host};Port={Port};Database={dbName};SearchPath={schema}";
+        $"User ID={User};Password={Password};Host={Host};Port={Port};Database={dbName};SearchPath={schema};CommandTimeout={CommandTimeout}";
 }
